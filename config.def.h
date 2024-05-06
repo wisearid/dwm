6212d6dc36fdf2e19f4e85c46e4902dcc2f46f31
Sam <95317180+wisearid@users.noreply.github.com>
2024-05-06 15:33:06 -0400

    bug fix

--- config.def.h
+++ config.def.h
@@ -62,7 +62,7 @@ static const char *termcmd[]  = { "kitty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_i,      spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
