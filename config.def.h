db163d8bc86674673655820721e4fb22095518e0
Sam <95317180+wisearid@users.noreply.github.com>
2024-06-01 08:15:38 -0400

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono:size=16:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Ubuntu Mono:size=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Ubuntu Mono:size=12:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "󰺵", "", "", "󰭹", "", "", "" };
+static const char *tags[] = { "", "", "󰺵", "", "", "󰭹", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
