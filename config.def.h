6a0448b1af0ffdbb73b60f8d348ffb99bff408e1
Sam <95317180+wisearid@users.noreply.github.com>
2024-07-12 04:34:34 +0000

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -41,9 +41,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "T",      tile },    /* first entry is default */
+	{ "N",      NULL },    /* no layout function means floating behavior */
+	{ "M",      monocle },
 };
 
 /* key definitions */
